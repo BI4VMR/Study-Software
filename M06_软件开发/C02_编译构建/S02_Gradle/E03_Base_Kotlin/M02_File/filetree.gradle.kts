c987a168a3e3ff1efe4f5b850b861e81d0f5b9e0
BI4VMR <bi4vmr@outlook.com>
2025-07-18 18:02:48 +0800

    A1232920-1C57-479B-AF9C-CE97231CF87A

---
+++ M06_软件开发/C02_编译构建/S02_Gradle/E03_Base_Kotlin/M02_File/filetree.gradle.kts
@@ -0,0 +1,61 @@
+// 调用测试方法
+example01()
+
+
+fun example01() {
+    // 创建FileTree实例，指定根目录为 `<当前模块>/testFiles/` 。
+    val baseDir = "${layout.projectDirectory}${File.separator}testFiles"
+    val tree: FileTree = fileTree(baseDir)
+    // 查看匹配到的文件
+    tree.forEach {
+        println("${it.path}")
+    }
+}
+
+fun example02() {
+    // 设置过滤规则
+    val filter: List<String> = listOf(
+        "10?.log",
+        "**/*Test*",
+    )
+
+    // 创建FileTree实例，指定根目录为 `<当前模块>/testFiles/` 。
+    val baseDir = "${layout.projectDirectory}${File.separator}testFiles"
+    val tree: FileTree = fileTree(baseDir) {
+        // 保留命中规则的文件
+        include(filter)
+    }
+    // 查看匹配到的文件
+    tree.forEach {
+        println("${it.path}")
+    }
+}
+
+/**
+ * 排除测试目录下的所有Log文件，但要保留 `20250701.log` 。
+ */
+fun example03() {
+    val baseDir = "${layout.projectDirectory}${File.separator}testFiles"
+    val tree: FileTree = fileTree(baseDir) {
+        include("20250701.log")
+        exclude("*.log")
+    }
+    // 查看匹配到的文件
+    tree.forEach {
+        println("${it.path}")
+    }
+}
+
+/**
+ * 排除测试目录下的所有Log文件，但要保留 `20250701.log` 。
+ */
+fun example03_2() {
+    val baseDir = "${layout.projectDirectory}${File.separator}testFiles"
+    // 分别使用排除和包含规则创建FileTree实例，再将二者合并。
+    val tree: FileTree = fileTree(baseDir) { exclude("*.log") } +
+            fileTree(baseDir) { include("20250701.log") }
+    // 查看匹配到的文件
+    tree.forEach {
+        println("${it.path}")
+    }
+}
---
+++ M06_软件开发/C02_编译构建/S02_Gradle/E03_Base_Kotlin/settings.gradle.kts
@@ -0,0 +1,5 @@
+/* ----- 工程结构声明 ----- */
+// 主工程名称
+rootProject.name = "E03_Base_Kotlin"
+
+include(":M02_File")
